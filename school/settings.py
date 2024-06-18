3b4ec1a37365529723944734d1e70a3f6e7544c3
Aber-Racheal <rachealaberr@gmail.com>
2024-06-18 17:11:17 +0300

    created an app

--- school/settings.py
+++ school/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'classroom',
-    'teacher',
-    'course',
+    # 'classroom',
+    # 'teacher',
+    # 'course',
     'student'
 ]
 
